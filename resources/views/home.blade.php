d2a9b6250ca020b4935477000d3a135798a29e0d
Alexander37ss <alexanderpalazuelos37@gmail.com>
2022-10-29 12:46:45 -0600

    Pagina

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+@extends('master')
+
+@section('contenido')
+<div class="jumbotron">
+    <h1 class="display-4">Mi Escuelita App</h1>
+    <p class="lead">Bienvenido a mi escuelita.edu!</p>
+    <hr class="my-4">
+    <p>
+        En esta escuela aprenderas a leer, escribir y dibujar.
+    </p>
+    <img src="{{asset('img/Escuelita.jpg')}}" class="img-thumbnail" alt="Cinque Terre" width="400px" >
+      
+@stop
