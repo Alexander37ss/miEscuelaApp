9947f28fb24f6057d9db7e177ce29f46a0ab2f44
Alexander37ss <alexanderpalazuelos37@gmail.com>
2022-11-05 12:32:39 -0700

    Editar Registro

--- app/Http/Controllers/AlumnoController.php
+++ app/Http/Controllers/AlumnoController.php
@@ -27,4 +27,23 @@ class AlumnoController extends Controller
 
         return redirect('/alumnos');
     }
+    function editar($id){
+        $alumno = Alumno::find($id);
+
+        return view('/alumnos.editar', compact('alumno'));
+    }
+
+    function actualizar(Request $datos, $id){
+        $alumno = Alumno::find($id);
+        $alumno->n_control = $datos->input('n_control');
+        $alumno->nombre = $datos->input('nombre');
+        $alumno->edad = $datos->input('edad');
+        $alumno->sexo = $datos->input('sexo');
+        $alumno->fecha_nacimiento = $datos->input('fecha_nacimiento');
+        $alumno->domicilio = $datos->input('domicilio');
+        $alumno->Telefono = $datos->input('Telefono');
+        $alumno->save();
+
+        return redirect('/alumnos');
+    }
 }
--- resources/views/Alumnos/registrar.blade.php
+++ resources/views/Alumnos/registrar.blade.php
@@ -6,30 +6,30 @@
         @csrf
         <div class="form-group">
             <label for="">No. Control:</label>
-            <input type="text" placeholder="Introduce tu numero de control" class="form-control" name="n_control" required>
+            <input type="text" class="form-control" name="n_control" required>
         </div>
         <div class="form-group">
             <label for="">Nombre:</label>
-            <input type="text" placeholder="Introduce tu nombre completo" class="form-control" name="nombre" required>
+            <input type="text" class="form-control" name="nombre" required>
         </div>
         <div class="form-group">
             <label for="">Edad:</label>
-            <input type="number" placeholder="Introduce tu edad" class="form-control" name="edad" required>
+            <input type="number" class="form-control" name="edad" required>
         </div>
         <label for="">Sexo:</label><br>
         <input type="radio" name="sexo" value="1"> Másculino <br>
         <input type="radio" name="sexo" value="0"> Femenino <br><br>
         <div class="form-group">
-            <label for="">Fecha de nacimiento:</label>
+            <label for="">Edad:</label>
             <input type="date" class="form-control" name="fecha_nacimiento" required>
         </div>
         <div class="form-group">
             <label for="">Domicilio:</label>
-            <textarea name="domicilio" placeholder="Introduce tu domicilio" cols="30" rows="10" class="form-control"></textarea>
+            <textarea name="domicilio" cols="30" rows="10" class="form-control"></textarea>
         </div>
         <div class="form-group">
             <label for="">Teléfono:</label>
-            <input type="text"  placeholder="Introduce tu teléfono" class="form-control" name="telefono" required>
+            <input type="text" class="form-control" name="telefono" required>
         </div>
         <div>
             <input type="submit" value="Registrar" class="btn btn-primary">
